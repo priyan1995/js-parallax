002c44edb0fdde22dbec1e0512413d4eab9fa016
priyan <45042030+priyan1995@users.noreply.github.com>
2021-05-30 22:09:34 +0530

    images optimized

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 
 
 let sun = document.getElementById('subShined');
-
+let backMountain = document.getElementById('backMountain');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     sun.style.top = value * 0.08 + '%';
+    backMountain.style.top = value * 0.05 + '%';
 
 })
