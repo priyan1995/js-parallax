5777d058758f9ea2fcd8f75cd37a5ed4d2fa6396
priyan <45042030+priyan1995@users.noreply.github.com>
2021-05-30 22:35:57 +0530

    clouds added

--- main.js
+++ main.js
@@ -3,10 +3,16 @@
 
 let sun = document.getElementById('subShined');
 let backMountain = document.getElementById('backMountain');
+let cloud1 = document.getElementById('cloud1');
+let cloud2 = document.getElementById('cloud2');
+let cloud3 = document.getElementById('cloud3');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     sun.style.top = value * 0.08 + '%';
+    cloud1.style.top = value * 0.08 + '%';
+    cloud2.style.top = value * 0.08 + '%';
+    cloud3.style.top = value * 0.08 + '%';
     backMountain.style.top = value * 0.05 + '%';
 
 })
