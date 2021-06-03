76fce0e71ae330dd59bfdcb6e9e318140d3f158b
priyan <45042030+priyan1995@users.noreply.github.com>
2021-06-04 01:07:06 +0530

    birds added

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ let backMountain = document.getElementById('backMountain');
 let cloud1 = document.getElementById('cloud1');
 let cloud2 = document.getElementById('cloud2');
 let cloud3 = document.getElementById('cloud3');
+let bird1 = document.getElementById('bird');
+let bird2 = document.getElementById('bird2');
+let bird3 = document.getElementById('bird3');
+let bird4 = document.getElementById('bird4');
+
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
@@ -14,6 +19,12 @@ window.addEventListener('scroll', function(){
     cloud2.style.top = value * 0.08 + '%';
     cloud3.style.top = value * 0.08 + '%';
     backMountain.style.top = value * 0.05 + '%';
+    bird1.style.top = value * 0.08 + '%';
+    bird2.style.top = value * 0.08 + '%';
+    bird3.style.top = value * 0.08 + '%';
+    bird4.style.top = value * 0.08 + '%';
+
+
 
 })
 
